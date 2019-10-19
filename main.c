8b2709cdd7341c49ea47414c44174eabbb1feacc
ameerasherin98 <56751656+ameerasherin98@users.noreply.github.com>
2019-10-19 13:08:10 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
+void add(int a, int b)
+{
+    printf("The sum of given nos is %d",a+b);
+}
+void sub(int a, int b)
+{
+    printf("The difference of given nos is %d",a-b);
+}
+void mul(int a,int b)
+{
+    printf("The product of given nos is %d",a*b);
+}
 
-int main(void) {
-    printf("Hello Calculator\n");
-
-    return 0;
+void main() {
+    int a,b;
+    printf("Enter two nos\n");
+    scanf("%d %d",&a,b);
+    add(a,b);
+    sub(a,b);
+    mul(a,b);
 }
